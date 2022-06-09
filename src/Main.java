222d64282b4f9cc241b2a7090d4f704ae529eaae
Jaydesire <elgovsky@ya.ru>
2022-06-09 13:18:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+
+        System.out.println(
+                bins.sum("00110111", "0110"));
+
+        System.out.println(
+                bins.mult("00110111", "0110"));
+    }
+}
